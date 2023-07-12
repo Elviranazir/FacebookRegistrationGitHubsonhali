390c94e394228f96bcf435ab19393c5ff1110f7c
Elvira <elvira.nzr@gmail.com>
2023-07-11 23:10:30 -0400

    deneme3

--- src/facebookRegistration.java
+++ src/facebookRegistration.java
@@ -2,6 +2,7 @@ public class facebookRegistration {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("deneme2");
+        System.out.println("deneme3");
 
     }
 }
